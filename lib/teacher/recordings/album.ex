dce73340b3193a89bd5672fb9e435f57bbc50d8f
elixircasts-io <hello@elixircasts.io>
2023-09-04 21:39:29 -0700

    init commit

---
+++ lib/teacher/recordings/album.ex
@@ -0,0 +1,21 @@
+defmodule Teacher.Recordings.Album do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "albums" do
+    field :artist, :string
+    field :genre, :string
+    field :summary, :string
+    field :title, :string
+    field :year, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(album, attrs) do
+    album
+    |> cast(attrs, [:title, :artist, :summary, :year, :genre])
+    |> validate_required([:title, :artist, :summary, :year, :genre])
+  end
+end
